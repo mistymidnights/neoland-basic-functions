60827ed01ff72f2691da268560477ade5fa45485
mistymidnightss <mistydeveloping@gmail.com>
2022-07-13 21:29:20 +0200

    Iteración #2: Buscar la palabra más larga

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const sum = (numberOne, numberTwo) => {
     return (numberOne > numberTwo) ? numberOne : numberTwo;
 }
     console.log(sum(183, 267));
+
+
+/* Iteración #2: Buscar la palabra más larga */
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman molon', 'Captain M.'];
+const findLongestWord = (list) => {
+    let longestWord = "";
+    list.forEach((element) => {
+        longestWord = element.length > longestWord.length ? element : longestWord;
+    })
+    return longestWord;
+}
+
+    console.log(findLongestWord(avengers));  
