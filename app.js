181fe26a5bffab7787528b6c2888515284a38534
mistymidnightss <mistydeveloping@gmail.com>
2022-07-15 00:42:01 +0200

    Iteration #8: Contador de repeticiones

--- app.js
+++ app.js
@@ -124,3 +124,29 @@ const nameFinder = [
     }
     console.log(nameFinder.includes('Tony'));
     console.log(nameFinder.indexOf('Tony'));
+
+    
+/**Iteration #8: Contador de repeticiones**
+Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.  Puedes usar este array para probar tu función: */
+
+const counterWords = [
+    'code',
+    'repeat',
+    'eat',
+    'sleep',
+    'code',
+    'enjoy',
+    'sleep',
+    'code',
+    'enjoy',
+    'upgrade',
+    'code'
+];
+let totalCount = [];
+const repeatCounter = (elements) =>{
+    counterWords.forEach((element) => {
+        totalCount[element] = totalCount[element] + 1 || 1;
+    })
+    return totalCount;
+  }
+  console.log(repeatCounter(counterWords));
